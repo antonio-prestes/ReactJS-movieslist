85b6d7b16176e43d0797e0a95f4008165f487680
Diego <diegoantoniodasp@hotmail.com>
2021-06-27 21:48:46 -0300

    Line 7:23 fix link

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export const Header = () => (
     <div className="container">
         <header
             className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-            <Link to="/favorites" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+            <Link to="/" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
                 <h4>Movies List API</h4>
             </Link>
 
