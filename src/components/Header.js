57f7d006b733912f29b2ca6a3d3685a961835df4
Diego <diegoantoniodasp@hotmail.com>
2021-06-27 21:40:24 -0300

    fixed Line 7:16:  add Link to

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ export const Header = () => (
     <div className="container">
         <header
             className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-            <a href="/" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+            <Link to="/favorites" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
                 <h4>Movies List API</h4>
-            </a>
+            </Link>
 
             <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
                 <li><Link to="/" className="nav-link px-2 link-secondary">Home</Link></li>
